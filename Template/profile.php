717d0bf2fe2cff43a8c5c7707a639e61bbab6f5d
omego <mserihi@gmail.com>
2021-12-05 21:30:50 +0300

    Fixed: edit task select background

--- Template/profile.php
+++ Template/profile.php
@@ -3,7 +3,6 @@
     <div class="panel">
         <ul>
             <li><?= t('Login:') ?> <strong><?= $this->text->e($user['username']) ?></strong></li>
-            <li><?= t('Full Name:') ?> <strong><?= $this->text->e($user['name']) ?: t('None') ?></strong></li>
             <li><?= t('Email:') ?> <strong><?= $this->text->e($user['email']) ?: t('None') ?></strong></li>
             <?= $this->hook->render('template:user:show:profile:info', array('user' => $user)) ?>
         </ul>
